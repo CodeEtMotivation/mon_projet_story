5f3e8c60dd5196cc8b62c424953f6d4cd0f28eb8
code et motivation <judarmwil@gmail.com>
2023-02-06 20:09:53 +0100

    creation et configuration de mon projet blade/laravel/breeze

---
+++ resources/views/Home.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  @vite('resources/css/app.css')
+</head>
+<body>
+  <h1 class="text-bold text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:underline">
+    Hello world!
+  </h1>
+  <span class="text-bold text-gray-600 underline">
+    beererg
+  </span>
+</body>
+</html>
